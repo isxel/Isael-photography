c4b42996033a6f7562880e89ff004d471ccd917d
Isael <iramir17@stedwards.edu>
2024-08-03 00:18:06 -0500

    Added more to the about page and added a new image Included styling as well, still need to update.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,22 +1,38 @@
 import React from "react";
-import "./../styles/Footer.css";
+import "./../styles/About.css";
+import myImage from "../assets/img/myImage.jpeg";
 
 function About() {
   return (
-    <div>
-      <h2>About Me</h2>
-      <p>
-        Hello, I'm Isael Ramirez, a recent graduate with a degree in Computer
-        Science based in Austin, Texas. My passion for cars and the vibrant
-        culture surrounding them has been a significant part of my life. I
-        cherish the connections, joy, and memories that come from being immersed
-        in the automotive world. Photography has allowed me to capture these
-        moments and share them with others. For many, cars are more than just a
-        mode of transportation—they are a form of self-expression. I take great
-        pride in capturing the effort and creativity people pour into their
-        vehicles, preserving the essence of their individuality through my
-        camera lens.
-      </p>
+    <div className="about-container">
+      <div className="about-image">
+        <img src={myImage} alt="Isael Ramirez" />
+      </div>
+      <div className="about-text">
+        <h2>About Me</h2>
+        <p>
+          Hello, I'm Isael Ramirez, a recent graduate with a degree in Computer
+          Science based in Austin, Texas. My passion for cars and the vibrant
+          culture surrounding them has been a significant part of my life. I
+          cherish the connections, joy, and memories that come from being
+          immersed in the automotive world.
+        </p>
+        <p>
+          Growing up, I was surrounded by Japanese cars, especially the Acura
+          Integra, which sparked my love for automobiles. Currently, I drive a
+          1990 Mazda Miata, a car I've enjoyed immensely for the better part of
+          two years. The Miata has not only been a joy to drive but also a
+          canvas for my automotive creativity and passion.
+        </p>
+        <p>
+          Photography has allowed me to capture these moments and share them
+          with others. For many, cars are more than just a mode of
+          transportation—they are a form of self-expression. I take great pride
+          in capturing the effort and creativity people pour into their
+          vehicles, preserving the essence of their individuality through my
+          camera lens.
+        </p>
+      </div>
     </div>
   );
 }
