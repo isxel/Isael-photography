2e1073a20024e408303f85276f65decfbf9c3be8
Isael <iramir17@stedwards.edu>
2024-08-16 15:31:28 -0500

    config fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/isael-photography/",
+  base: "/Isael-photography/",
   plugins: [react()],
 });
